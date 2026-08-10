0e8a12c01657be7322ec281f1590b4e64c2674f8
Dipu Kumar <dipukumarpojhi@gmail.com>
2026-08-10 10:39:12 +0530

    Contact us icon used and clickable

--- src/components/contactList.jsx
+++ src/components/contactList.jsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { FaPhoneAlt, FaEnvelope, FaWhatsapp } from "react-icons/fa";
 
 function ContactList() {
   return (
@@ -25,7 +26,7 @@ function ContactList() {
               <p className="mb-1">
                 <strong>Address:</strong>
               </p>
-              <p className="text-muted mb-0" style={{ fontSize: "14px" }}>
+              <p className="mb-0" style={{ fontSize: "14px" }}>
                 ELECTRICAL BROTHERS GROUP, NEAR PATWARI, PRASHIKSRAN KENDRA, SURYA CHOWK LINGIYADIH,
                 BILASPUR 495006,
                 CHHATTISGARH.
@@ -45,13 +46,41 @@ function ContactList() {
                 marginBottom: "0",
               }}
             >
-              <p className="mb-1">
+              {/* <p className="mb-1">
                 <strong>Mobile Number:</strong>
               </p>
               <p className="text-muted mb-0" style={{ fontSize: "14px" }}>
                 8959164260 <br />
                 sandeepvastrakar14@gmail.com
-              </p>
+              </p> */}
+              <p className="mb-2 d-flex align-items-center">
+  <FaPhoneAlt className="me-2 text-primary" />
+  <a href="tel:8959164260" className="text-decoration-none text-dark">
+    8959164260
+  </a>
+</p>
+
+<p className="mb-2 d-flex align-items-center">
+  <FaEnvelope className="me-2 text-primary" />
+  <a
+    href="mailto:sandeepvastrakar14@gmail.com"
+    className="text-decoration-none text-dark"
+  >
+    sandeepvastrakar14@gmail.com
+  </a>
+</p>
+
+<p className="mb-0 d-flex align-items-center">
+  <FaWhatsapp className="me-2 text-success" />
+  <a
+    href="https://wa.me/918959164260"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="text-decoration-none text-dark"
+  >
+    8959164260
+  </a>
+</p>
             </div>
           </Col>
         </Row>
